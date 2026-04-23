f7ae52c9e3ad3d16bb1fa384d530d7fe4c208c83
Carles Jove i Buxeda <hola@carlus.cat>
2026-04-23 21:46:43 +0200

    Add option to include verb conjugations too

---
+++ verbs.js
@@ -0,0 +1,421 @@
+const verbs = [
+
+  {
+    "lessonNum": 2,
+    "lesson": 'El Verbo Izan (Ser) En Presente Y El Artículo En Euskera',
+    "lessonUrl": "https://kondaira.net/ikastaroa/Ikasgaia02.html",
+    "type": "verbs",
+    "name": 'Izan Aditza',
+    "eu": `ni naiz
+hi haiz
+hura da
+gu gara
+zu zara
+zuek zarete
+haiek dira`,
+    "es": `yo soy
+tú eres
+él es
+nosotros somos
+tú eres / usted es
+vosotros sois / ustedes son
+ellos son`
+  },
+
+  {
+    "lessonNum": 10,
+    "lesson": 'El Verbo Egon (Estar) En Presente Y La Declinación Non (Dónde) Del Euskera',
+    "lessonUrl": "https://kondaira.net/ikastaroa/Ikasgaia10.html",
+    "type": "verbs",
+    "name": 'Egon Aditza',
+    "eu": `ni nago
+hi hago
+hura dago
+gu gaude
+zu zaude
+zuek zaudete
+haiek daude`,
+    "es": `yo estoy
+tú estás
+él está, también significa hay
+nosotros estamos
+tú estás / usted está
+vosotros estáis / ustedes están
+ellos están, también significa hay`
+  },
+
+  {
+    "lessonNum": 11,
+    "lesson": 'El Verbo Bizi Izan (Vivir) En Presente Y Los Adverbios En Euskera',
+    "lessonUrl": "https://kondaira.net/ikastaroa/Ikasgaia11.html",
+    "type": "verbs",
+    "name": 'Bizi Aditza',
+    "eu": `ni bizi naiz
+hi bizi haiz
+hura bizi da
+gu bizi gara
+zu bizi zara
+zuek bizi zarete
+haiek bizi dira`,
+    "es": `yo vivo
+tú vives
+él vive
+nosotros vivimos
+tú vives / usted vive
+vosotros vivís / ustedes viven
+ellos viven`
+  },
+
+  {
+    "lessonNum": 12,
+    "lesson": 'El Verbo Etorri (Venir) En Presente Y La Declinación Nondik (Desde Dónde) Del Euskera',
+    "lessonUrl": "https://kondaira.net/ikastaroa/Ikasgaia12.html",
+    "type": "verbs",
+    "name": 'Etorri Aditza',
+    "eu": `ni nator
+hi hator
+hura dator
+gu gatoz
+zu zatoz
+zuek zatozte
+haiek datoz`,
+    "es": `yo vengo
+tú vienes
+él viene
+nosotros venimos
+tú vienes / usted viene
+vosotros venís / ustedes vienen
+ellos vienen`
+  },
+
+  {
+    "lessonNum": 13,
+    "lesson": 'El Verbo Joan (Ir) En Presente Y La Declinación Nora (A Dónde) Del Euskera',
+    "lessonUrl": "https://kondaira.net/ikastaroa/Ikasgaia13.html",
+    "type": "verbs",
+    "name": 'Joan Aditza',
+    "eu": `ni noa
+hi hoa
+hura doa
+gu goaz
+zu zoaz
+zuek zoazte
+haiek doaz`,
+    "es": `yo voy
+tú vas
+él va
+nosotros vamos
+tú vas / usted va
+vosotros vais / ustedes van
+ellos van`
+  },
+
+  {
+    "lessonNum": 14,
+    "lesson": 'El Verbo Ibili (Andar) En Presente Y La Declinación Norekin (Con Quién) Del Euskera',
+    "lessonUrl": "https://kondaira.net/ikastaroa/Ikasgaia14.html",
+    "type": "verbs",
+    "name": 'Ibili Aditza',
+    "eu": `ni nabil
+hi habil
+hura dabil
+gu gabiltza
+zu zabiltza
+zuek zabiltzate
+haiek dabiltza`,
+    "es": `yo ando
+tú andas
+él anda
+nosotros andamos
+tú andas / usted anda
+vosotros andáis / ustedes andan
+ellos andan`
+  },
+
+  {
+    "lessonNum": 20,
+    "lesson": 'El Pretérito Imperfecto Del Verbo Izan (Ser)',
+    "lessonUrl": "https://kondaira.net/ikastaroa/Ikasgaia20.html",
+    "type": "verbs",
+    "name": 'IZAN aditzaren lehenaldia',
+    "eu": `NI NINTZEN
+HI HINTZEN
+HURA ZEN
+GU GINEN
+ZU ZINEN
+ZUEK ZINETEN
+HAIEK ZIREN`,
+    "es": `Yo era
+Tú eras
+Él era
+Nosotros éramos
+Tú eras / Usted era
+Vosotros erais / Ustedes eran
+Ellos eran`
+  },
+
+  {
+    "lessonNum": 28,
+    "lesson": 'El Verbo Tener En Euskera En Presente A Través Del Verbo Edun',
+    "lessonUrl": "https://kondaira.net/ikastaroa/Ikasgaia28.html",
+    "type": "verbs",
+    "name": '*EDUN ADITZAREN ORAINALDIA - LEHEN ZATIA',
+    "eu": `NIK DUT
+HIK DUK / N
+HARK DU
+GUK DUGU
+ZUK DUZU
+ZUEK DUZUE
+HAIEK DUTE`,
+    "es": `Yo tengo
+Tú tienes
+Él tiene
+Nosotros tenemos
+Tú tienes / Usted tiene
+Vosotros tenéis / Ustedes tienen
+Ellos tienen`
+  },
+
+  {
+    "lessonNum": 30,
+    "lesson": 'Tener En Euskera En Presente A Través Del Verbo Edun - Segunda Parte',
+    "lessonUrl": "https://kondaira.net/ikastaroa/Ikasgaia30.html",
+    "type": "verbs",
+    "name": '*EDUN ADITZAREN ORAINALDIA - BIGARREN ZATIA',
+    "eu": `NIK DITUT
+HIK DITUK / N
+HARK DITU
+GUK DITUGU
+ZUK DITUZU
+ZUEK DITUZUE
+HAIEK DITUZTE`,
+    "es": `Yo tengo
+Tú tienes
+Él tiene
+Nosotros tenemos
+Tú tienes / Usted tiene
+Vosotros tenéis / Ustedes tienen
+Ellos tienen`
+  },
+
+  {
+    "lessonNum": 36,
+    "lesson": 'El Verbo Tener En Pretérito Imperfecto A Través Del Verbo Edun',
+    "lessonUrl": "https://kondaira.net/ikastaroa/Ikasgaia36.html",
+    "type": "verbs",
+    "name": '*EDUN ADITZAREN LEHENALDIA - LEHEN ZATIA',
+    "eu": `NIK NUEN
+HIK HUEN
+HARK ZUEN
+GUK GENUEN
+ZUK ZENUEN
+ZUEK ZENUTEN
+HAIEK ZUTEN`,
+    "es": `Yo tenía
+Tú tenías
+Él tenía
+Nosotros teníamos
+Tú tenías / Usted tenía
+Vosotros teníais / Ustedes tenían
+Ellos tenían`
+  },
+
+  {
+    "lessonNum": 36,
+    "lesson": 'El Verbo Tener En Pretérito Imperfecto A Través Del Verbo Edun',
+    "lessonUrl": "https://kondaira.net/ikastaroa/Ikasgaia36.html",
+    "type": "verbs",
+    "name": '*EDUN ADITZAREN LEHENALDIA - BIGARREN ZATIA',
+    "eu": `NIK NITUEN
+HIK HITUEN
+HARK ZITUEN
+GUK GENITUEN
+ZUK ZENITUEN
+ZUEK ZENITUZTEN
+HAIEK ZITUZTEN`,
+    "es": `Yo tenía
+Tú tenías
+Él tenía
+Nosotros teníamos
+Tú tenías / Usted tenía
+Vosotros teníais / Ustedes tenían
+Ellos tenían`
+  },
+
+  {
+    "lessonNum": 41,
+    "lesson": 'El Verbo Esan (Decir) En Presente Y Las Oraciones Completivas En Euskera A Través Del Sufijo -La',
+    "lessonUrl": "https://kondaira.net/ikastaroa/Ikasgaia41.html",
+    "type": "verbs",
+    "name": 'ESAN ADITZAREN ORAINALDIA',
+    "eu": `NIK DIOT
+HIK DIOK / N
+HARK DIO
+GUK DIOGU
+ZUK DIOZU
+ZUEK DIOZUE
+HAIEK DIOTE`,
+    "es": `Yo digo
+Tú dices
+Él dice
+Nosotros decimos
+Tú dices / Usted dice
+Vosotros decís / Ustedes dicen
+Ellos dicen`
+  },
+
+  {
+    "lessonNum": 43,
+    "lesson": 'El Verbo Jakin (Saber) En Presente',
+    "lessonUrl": "https://kondaira.net/ikastaroa/Ikasgaia43.html",
+    "type": "verbs",
+    "name": 'JAKIN ADITZAREN ORAINALDIA',
+    "eu": `NIK DAKIT
+HIK DAKIK / N
+HARK DAKI
+GUK DAKIGU
+ZUK DAKIZU
+ZUEK DAKIZUE
+HAIEK DAKITE`,
+    "es": `Yo sé
+Tú sabes
+Él sabe
+Nosotros sabemos
+Tú sabes / Usted sabe
+Vosotros sabéis / Ustedes saben
+Ellos saben`
+  },
+
+  {
+    "lessonNum": 45,
+    "lesson": 'El Verbo Eduki (Tener) En Presente Y Los Grados Del Adjetivo En Euskera: "Más...", "El Más..." Y "Demasiado..."',
+    "lessonUrl": "https://kondaira.net/ikastaroa/Ikasgaia45.html",
+    "type": "verbs",
+    "name": 'EDUKI ADITZAREN ORAINALDIA - LEHEN ZATIA',
+    "eu": `NIK DAUKAT
+HIK DAUKAK / N
+HARK DAUKA
+GUK DAUKAGU
+ZUK DAUKAZU
+ZUEK DAUKAZUE
+HAIEK DAUKATE`,
+    "es": `Yo tengo
+Tú tienes
+Él tiene
+Nosotros tenemos
+Tú tienes / Usted tiene
+Vosotros tenéis / Ustedes tienen
+Ellos tienen`
+  },
+
+  {
+    "lessonNum": 45,
+    "lesson": 'El Verbo Eduki (Tener) En Presente Y Los Grados Del Adjetivo En Euskera: "Más...", "El Más..." Y "Demasiado..."',
+    "lessonUrl": "https://kondaira.net/ikastaroa/Ikasgaia45.html",
+    "type": "verbs",
+    "name": 'EDUKI ADITZAREN ORAINALDIA - BIGARREN ZATIA',
+    "eu": `NIK DAUZKAT
+HIK DAUZKAK / N
+HARK DAUZKA
+GUK DAUZKAGU
+ZUK DAUZKAZU
+ZUEK DAUZKAZUE
+HAIEK DAUZKATE`,
+    "es": `Yo tengo
+Tú tienes
+Él tiene
+Nosotros tenemos
+Tú tienes / Usted tiene
+Vosotros tenéis / Ustedes tienen
+Ellos tienen`
+  },
+
+  {
+    "lessonNum": 64,
+    "lesson": 'El Verbo Eraman (Llevar) En Presente',
+    "lessonUrl": "https://kondaira.net/ikastaroa/Ikasgaia64.html",
+    "type": "verbs",
+    "name": 'ERAMAN ADITZAREN ORAINALDIA - LEHEN ZATIA',
+    "eu": `NIK DARAMAT
+HIK DARAMAK / N
+HARK DARAMA
+GUK DARAMAGU
+ZUK DARAMAZU
+ZUEK DARAMAZUE
+HAIEK DARAMATE`,
+    "es": `Yo llevo
+Tú llevas
+Él lleva
+Nosotros llevamos
+Tú llevas / Usted lleva
+Vosotros lleváis / Ustedes llevan
+Ellos llevan`
+  },
+
+  {
+    "lessonNum": 64,
+    "lesson": 'El Verbo Eraman (Llevar) En Presente',
+    "lessonUrl": "https://kondaira.net/ikastaroa/Ikasgaia64.html",
+    "type": "verbs",
+    "name": 'ERAMAN ADITZAREN ORAINALDIA - BIGARREN ZATIA',
+    "eu": `NIK DARAMATZAT
+HIK DARAMATZAK / N
+HARK DARAMATZA
+GUK DARAMATZAGU
+ZUK DARAMATZAZU
+ZUEK DARAMATZAZUE
+HAIEK DARAMATZATE`,
+    "es": `Yo llevo
+Tú llevas
+Él lleva
+Nosotros llevamos
+Tú llevas / Usted lleva
+Vosotros lleváis / Ustedes llevan
+Ellos llevan`
+  },
+
+  {
+    "lessonNum": 65,
+    "lesson": 'El Verbo Ekarri (Traer) En Presente Y Las Diferentes Formas De Construir El Gerundio En Euskera',
+    "lessonUrl": "https://kondaira.net/ikastaroa/Ikasgaia65.html",
+    "type": "verbs",
+    "name": 'EKARRI ADITZAREN ORAINALDIA - LEHEN ZATIA',
+    "eu": `NIK DAKART
+HIK DAKARK / NA
+HARK DAKAR
+GUK DAKARGU
+ZUK DAKARZU
+ZUEK DAKARZUE
+HAIEK DAKARTE`,
+    "es": `Yo traigo
+Tú traes
+Él trae
+Nosotros traemos
+Tú traes / Usted trae
+Vosotros traéis / Ustedes traen
+Ellos traen`
+  },
+
+  {
+    "lessonNum": 65,
+    "lesson": 'El Verbo Ekarri (Traer) En Presente Y Las Diferentes Formas De Construir El Gerundio En Euskera',
+    "lessonUrl": "https://kondaira.net/ikastaroa/Ikasgaia65.html",
+    "type": "verbs",
+    "name": 'EKARRI ADITZAREN ORAINALDIA - BIGARREN ZATIA',
+    "eu": `NIK DAKARTZAT
+HIK DAKARTZAK / N
+HARK DAKARTZA
+GUK DAKARTZAGU
+ZUK DAKARTZAZU
+ZUEK DAKARTZAZUE
+HAIEK DAKARTZATE`,
+    "es": `Yo traigo
+Tú traes
+Él trae
+Nosotros traemos
+Tú traes / Usted trae
+Vosotros traéis / Ustedes traen
+Ellos traen`
+  },
+
+];
